a913f5b2ecf1854c6fa5fd95f37a015c17d43b82
Seungmin Lee <77082806+BaboJayden@users.noreply.github.com>
2024-11-15 23:53:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,24 @@ const html = {
     store : document.getElementById("store"),
     items : document.getElementById("items"),
     dict : document.getElementById("dict"),
-    useoil : document.getElementById("useoil")
+    useoil : document.getElementById("useoil"),
+    level : document.getElementById("level"),
+    s_cost : document.getElementById("s_cost"),
+    value : document.getElementById("value"),
+    s_item : document.getElementById("s_item"),
+    currentcoin : document.getElementById("currentcoin"),
+    probability : document.getElementById("probability")
 }
 
 const changeImg = lv => {
     image.src = `bean${lv}.png`;
     image.alt = `${lv}단계 : ${Bean[lv-1].name}`
+    html.level.innerHTML = `${lv}단계 : ${Bean[lv-1].name}`
+    html.s_cost.innerHTML = `강화 비용 : ${Bean[lv-1].s_cost}코인`
+    html.s_item.innerHTML = `강화 아이템 : ${Bean[lv-1].s_item}`
+    html.value.innerHTML = `판매 가격 : ${Bean[lv-1].value}코인`
+    html.currentcoin.innerHTML = `보유 코인 : ${coin}코인`
+    html.probability.innerHTML = `강화 확률 : ${100*Bean[lv-1].prob}%`
 }
 
 // EventListeners
@@ -37,9 +49,11 @@ image.addEventListener("click", () => {
         if (strengthen(check) == undefined){
 
         } else if (strengthen(check)){
+            coin -= Bean[level-1].s_cost;
             level++;
             changeImg(level)
         } else if (Bean[level-1].protect <= items.cake) {
+            coin -= Bean[level-1].s_cost;
             if (confirm(`강화에 실패했습니다.\n인절미 ${Bean[level-1].protect}개를 사용하시겠습니까?`)) {
                 useCake()
             } else {
@@ -48,6 +62,7 @@ image.addEventListener("click", () => {
                 changeImg(level)
             }
         } else {
+            coin -= Bean[level-1].s_cost;
             items.powder += givePowder()
             level = 1;
             changeImg(level)
@@ -56,6 +71,11 @@ image.addEventListener("click", () => {
     } else if (level == 25) {
         // 엔딩
     } 
+})
+
+document.getElementById("sellbean").addEventListener("click", () => {
+    sellBean();
+    changeImg(level);
 })
 
 // Functions
